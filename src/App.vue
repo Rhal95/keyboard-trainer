dd0b1112665c42ac7215aad4e2cef2e294522b68
Rhal95 <rhal9858@gmail.com>
2024-06-24 20:03:34 +0200

    fix build

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
 import {useGame} from "./store.ts";
-import {computed, ref, watch, watchEffect, watchPostEffect} from "vue";
+import {ref, watchEffect} from "vue";
 import WordCard from "./components/WordCard.vue";
 
 const game = useGame();
 const inputContent = ref('');
-const inputRef = ref(null);
+const inputRef = ref<HTMLInputElement | null>(null);
 
-watchEffect(()=>{
-  if(game.activeWord == null){
+watchEffect(() => {
+  if (game.activeWord == null) {
     inputContent.value = '';
   }
 });
 
-function classOfWord(word: string){
-  if (game.completedWords.includes(word)){
+function classOfWord(word: string) {
+  if (game.completedWords.includes(word)) {
     return 'completed';
   }
-  if (game.activeWord === word){
+  if (game.activeWord === word) {
     return 'active';
   }
   return '';
@@ -25,22 +25,29 @@ function classOfWord(word: string){
 
 const now = ref(Date.now());
 
-setInterval(()=>{now.value = Date.now()}, 100);
+setInterval(() => {
+  now.value = Date.now()
+}, 100);
 
 </script>
 
 <template>
   <main>
-    <button v-if="!game.running" @click="()=>{game.startGame(); inputRef.focus()}">Start Game</button>
-    <output>Score: {{game.score}}</output>
-    <time v-if="game.running">{{(now - game.startTime) / 1000}}s</time>
-    <time v-else-if="game.endTime">{{(game.endTime - game.startTime) / 1000}}s</time>
+    <button v-if="!game.running" @click="()=>{game.startGame(); inputRef?.focus()}">Start Game</button>
+    <output>Score: {{ game.score }}</output>
+    <time v-if="game.running">{{ (now - game.startTime) / 1000 }}s</time>
+    <time v-else-if="game.endTime">{{ (game.endTime - game.startTime) / 1000 }}s</time>
     <ul>
-      <li v-for="word in game.words.filter(w=>!game.completedWords.includes(w))" :class="classOfWord(word)"><WordCard :word="word" :currentInput="inputContent"/></li>
+      <li v-for="word in game.words.filter(w=>!game.completedWords.includes(w))" :class="classOfWord(word)">
+        <WordCard :word="word" :currentInput="inputContent"/>
+      </li>
     </ul>
-    <input ref="inputRef" type="text" :value="inputContent" @input="e=>{inputContent = e.target.value.trimStart(); game.checkWord(inputContent);}">
+    <input ref="inputRef" type="text" :value="inputContent"
+           @input="e=>{inputContent = (e.target as HTMLInputElement).value.trimStart(); game.checkWord(inputContent);}">
     <ul>
-      <li v-for="word in game.words.filter(w=>game.completedWords.includes(w))" :class="classOfWord(word)"><WordCard :word="word" :currentInput="inputContent"/></li>
+      <li v-for="word in game.words.filter(w=>game.completedWords.includes(w))" :class="classOfWord(word)">
+        <WordCard :word="word" :currentInput="inputContent"/>
+      </li>
     </ul>
   </main>
 </template>
@@ -58,14 +65,16 @@ main > ul {
   all: unset;
   display: flex;
   flex-direction: column-reverse;
-  grid: repeat(2, 1fr) / repeat(5,1fr);
+  grid: repeat(2, 1fr) / repeat(5, 1fr);
   align-content: center;
   justify-items: center;
   gap: 0.5rem;
 }
+
 main > ul:last-child {
   flex-direction: column;
 }
+
 main > ul > li {
   all: unset;
   border: 1px solid gray;
@@ -83,6 +92,7 @@ main > ul > li.active {
 main > ul > li.completed {
   background-color: palegreen;
 }
+
 input {
   font-size: large;
   padding: 0.25rem 0.5rem;
